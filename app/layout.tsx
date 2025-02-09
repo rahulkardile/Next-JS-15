a9d2de6db639621604fdc604c325644ab6e32c37
rahulkardile <rahulkardile321@gmail.com>
2025-02-09 06:28:41 +0530

    custom layout and layout is done

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <h2>Root Layout</h2>
         {children}
       </body>
     </html>
